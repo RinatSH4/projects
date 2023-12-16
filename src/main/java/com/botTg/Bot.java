2eef5d47c481c86d7a3b95fcf9c82305f30f9509
MacBook Pro <macbookpro@RinatSH.local>
2023-12-16 20:11:07 +0500

    update

--- src/main/java/com/botTg/Bot.java
+++ src/main/java/com/botTg/Bot.java
@@ -5,6 +5,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
@@ -52,7 +54,7 @@ public class Bot extends TelegramLongPollingBot {
 
             //Получаем текст сообщения пользователя, отправляем
             // в написанный нами обработчик
-            String responce = parseMessage(inMess.getText()) + "\n\n" + getTemperature();
+            String responce = parseMessage(inMess.getText(), chatId);
             System.out.println("Текст пользователя: " + inMess.getText());
 
             //Создаем объект класса SendMessage
@@ -63,42 +65,43 @@ public class Bot extends TelegramLongPollingBot {
             sendMessage.setReplyMarkup(replyKeyboardMarkup);
             System.out.println("Ответ пользователю: " + responce);
 
-            //отправка в чат
             try {
                 execute(sendMessage);
             } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
     }
 
-    public String parseMessage(String textMsg) {
+    public String parseMessage(String textMsg, String chatId) {
         String responce;
-        if(textMsg.equals("/start") || textMsg.equals("Перезапустить бота"))
-            responce = "Это гороскоп на каждый день, типа как тестовый бот, жми на свой знак и узнавай что тебя ждет сегодня! \nСегодня: " + date;
-        else if (textMsg.equals("Водолей"))
+        if(textMsg.equals("/start") || textMsg.equals("Перезапустить бота")) {
+            responce = "";
+            sendPhoto(chatId);
+        }
+        else if (textMsg.contains("Водолей"))
             responce = horoscope.parseURL("https://besthoro.ru/vodolej/tochnyj-goroskop-na-segodnya-vodolej.html");
-        else if (textMsg.equals("Рыбы"))
+        else if (textMsg.contains("Рыбы"))
             responce = horoscope.parseURL("https://besthoro.ru/ryby/tochnyj-goroskop-na-segodnya-ryby.html");
-        else if (textMsg.equals("Овен"))
+        else if (textMsg.contains("Овен"))
             responce = horoscope.parseURL("https://besthoro.ru/oven/tochnyj-goroskop-na-segodnya-oven.html");
-        else if (textMsg.equals("Телец"))
+        else if (textMsg.contains("Телец"))
             responce = horoscope.parseURL("https://besthoro.ru/telec/tochnyj-goroskop-na-segodnya-telec.html");
-        else if (textMsg.equals("Близнецы"))
+        else if (textMsg.contains("Близнецы"))
             responce = horoscope.parseURL("https://besthoro.ru/bliznecy/tochnyj-goroskop-na-segodnya-bliznecy.html");
-        else if (textMsg.equals("Рак"))
+        else if (textMsg.contains("Рак"))
             responce = horoscope.parseURL("https://besthoro.ru/rak/tochnyj-goroskop-na-segodnya-rak.html");
-        else if (textMsg.equals("Лев"))
+        else if (textMsg.contains("Лев"))
             responce = horoscope.parseURL("https://besthoro.ru/lev/tochnyj-goroskop-na-segodnya-lev.html");
-        else if (textMsg.equals("Дева"))
+        else if (textMsg.contains("Дева"))
             responce = horoscope.parseURL("https://besthoro.ru/deva/tochnyj-goroskop-na-segodnya-deva.html");
-        else if (textMsg.equals("Весы"))
+        else if (textMsg.contains("Весы"))
             responce = horoscope.parseURL("https://besthoro.ru/vesy/tochnyj-goroskop-na-segodnya-vesy.html");
-        else if (textMsg.equals("Скорпион"))
+        else if (textMsg.contains("Скорпион"))
             responce = horoscope.parseURL("https://besthoro.ru/skorpion/tochnyj-goroskop-na-segodnya-skorpion.html");
-        else if (textMsg.equals("Стрелец"))
+        else if (textMsg.contains("Стрелец"))
             responce = horoscope.parseURL("https://besthoro.ru/strelec/tochnyj-goroskop-na-segodnya-strelec.html");
-        else if (textMsg.equals("Козерог"))
+        else if (textMsg.contains("Козерог"))
             responce = horoscope.parseURL("https://besthoro.ru/kozerog/tochnyj-goroskop-na-segodnya-kozerog.html");
         else
             responce = "Неизвестная команда";
@@ -119,25 +122,29 @@ public class Bot extends TelegramLongPollingBot {
         KeyboardRow keyboardRow = new KeyboardRow();
         KeyboardRow keyboardRow1 = new KeyboardRow();
         KeyboardRow keyboardRow2 = new KeyboardRow();
+        KeyboardRow keyboardRow3 = new KeyboardRow();
+
         keyboardRows.add(keyboardRow);
         //Добавляем кнопки с текстом
-        keyboardRow.add(new KeyboardButton("Водолей"));
-        keyboardRow.add(new KeyboardButton("Рыбы"));
-        keyboardRow.add(new KeyboardButton("Овен"));
-        keyboardRow.add(new KeyboardButton("Телец"));
-        keyboardRow.add(new KeyboardButton("Близнецы"));
-        keyboardRow.add(new KeyboardButton("Рак"));
+        keyboardRow.add(new KeyboardButton("Водолей ♒"));
+        keyboardRow.add(new KeyboardButton("Рыбы ♓"));
+        keyboardRow.add(new KeyboardButton("Овен ♈"));
+        keyboardRow.add(new KeyboardButton("Телец ♉"));
 
         keyboardRows.add(keyboardRow1);
-        keyboardRow1.add(new KeyboardButton("Лев"));
-        keyboardRow1.add(new KeyboardButton("Дева"));
-        keyboardRow1.add(new KeyboardButton("Весы"));
-        keyboardRow1.add(new KeyboardButton("Скорпион"));
-        keyboardRow1.add(new KeyboardButton("Стрелец"));
-        keyboardRow1.add(new KeyboardButton("Козерог"));
+        keyboardRow1.add(new KeyboardButton("Близнецы ♊"));
+        keyboardRow1.add(new KeyboardButton("Рак ♋"));
+        keyboardRow1.add(new KeyboardButton("Лев ♌"));
+        keyboardRow1.add(new KeyboardButton("Дева ♍"));
 
         keyboardRows.add(keyboardRow2);
-        keyboardRow2.add(new KeyboardButton("Перезапустить бота"));
+        keyboardRow2.add(new KeyboardButton("Весы ♎"));
+        keyboardRow2.add(new KeyboardButton("Скорпион ♏"));
+        keyboardRow2.add(new KeyboardButton("Стрелец ♐"));
+        keyboardRow2.add(new KeyboardButton("Козерог ♑"));
+
+        keyboardRows.add(keyboardRow3);
+        keyboardRow3.add(new KeyboardButton("Перезапустить бота"));
 
         //добавляем лист с одним рядом кнопок в главный объект
         replyKeyboardMarkup.setKeyboard(keyboardRows);
@@ -149,12 +156,26 @@ public class Bot extends TelegramLongPollingBot {
             Document doc = Jsoup.connect(url).get();
             Elements element = doc.getElementsByClass("weather-value");
             Elements element1 = doc.getElementsByClass("measure");
-            String e1 = element.text().substring(0, element.text().indexOf(' '));
-            String e2 = element1.text().substring(0, element.text().indexOf(' '));;
+            String e1 = element.text().substring(0, element.text().indexOf(' ')+1);
+            String e2 = element1.text().substring(0, element.text().indexOf(' ')+1);;
             String temperature = "\nПогода в Энгельсе: " + e1 + "\nпо ощущению: " + e2;
             return temperature;
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    void sendPhoto(String chatId) {
+        SendPhoto send = new SendPhoto();
+        send.setChatId(chatId);
+        send.setPhoto(new InputFile("https://u.9111s.ru/uploads/202103/25/e2f07c9a3fa64f1c96c0b79716f1e4db.jpg"));
+        send.setCaption("Это гороскоп на каждый день, " +
+                "жми на свой знак и узнавай что тебя ждет сегодня! \nСегодня: " + date +
+                "\n\n" + getTemperature());
+        try {
+            execute(send);
+        } catch (TelegramApiException e) {
+            System.out.println(e);
         }
     }
 }
--- src/main/java/com/botTg/Main.java
+++ src/main/java/com/botTg/Main.java
@@ -4,6 +4,10 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         try {
